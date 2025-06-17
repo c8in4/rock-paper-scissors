aaa9d3107467650d5ff15f9e2fb5a0efcb835509
c8in4 <106409156+c8in4@users.noreply.github.com>
2025-06-17 20:47:50 +1200

    Remake RPS - console version

--- script.js
+++ script.js
@@ -1,93 +1,83 @@
-let computerChoice = "";
-let userChoice = "";
+let humanScore = 0
+let computerScore = 0
 
-let computerScore = 0;
-let userScore = 0;
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3)
 
-const buttons = document.querySelector("#buttons");
-const roundInfo = document.querySelector("#info");
-const computerScoreText = document.querySelector("#computerScoreText");
-const userScoreText = document.querySelector("#userScoreText");
-
-
-buttons.addEventListener("click", (event) => {
-    let choice = event.target.name;
-    if (choice === "Rock"
-        || choice === "Paper"
-        || choice === "Scissors"
-    ) {
-        playRound(choice);
+    switch (computerChoice) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors'
+        default:
+            console.error("Something went wrong with the computer's Choice.")
     }
-});
+}
 
-function playRound(rpsButton) {
-    computerChoice = getRandomChoice();
-    console.log(`Computer's choice: ${computerChoice}`);
+function getHumanChoice() {
+    let humanChoice = ''
+    do {
+        humanChoice = prompt('Type in "Rock", "Paper" or "Scissors".')
+    }
+    while (!(capitalizeWord(humanChoice) == 'Rock' || 'Paper' || 'Scissors'))
 
-    userChoice = rpsButton;
-    console.log(`User's choice: ${userChoice}`);
+    return capitalizeWord(humanChoice)
+}
 
-    compareChoices(userChoice, computerChoice);
-};
+function capitalizeWord(word) {
+    const lowerCaseWord = word.toLowerCase()
+    const transformedWord = lowerCaseWord.charAt(0).toUpperCase() + lowerCaseWord.slice(1)
+    return transformedWord
+}
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+    console.log(`Human's choice: ${humanChoice}\nComputer's choice: ${computerChoice}`)
+
+    let winner = ''
+
+    if (
+        (humanChoice == 'Rock' && computerChoice == 'Paper') ||
+        (humanChoice == 'Paper' && computerChoice == 'Scissors') ||
+        (humanChoice == 'Scissors' && computerChoice == 'Rock')
+    ) {
+        winner = 'Computer'
+        computerScore++
+    } else if (
+        (computerChoice == 'Rock' && humanChoice == 'Paper') ||
+        (computerChoice == 'Paper' && humanChoice == 'Scissors') ||
+        (computerChoice == 'Scissors' && humanChoice == 'Rock')
+    ) {
+        winner = 'Human'
+        humanScore++
+    } else {
+        winner = 'Nobody'
+    }
+
+    if (winner != 'Nobody') console.log(`${winner} won.`)
+    return winner
+}
+
+function playGame() {
+    while (humanScore < 5 && computerScore < 5) {
+        playRound()
+        console.log(`Human: ${humanScore}\nComputer: ${computerScore}`)
+    }
+    finishGame()
+}
+
+function finishGame() {
+    if (humanScore == 5) {
+        alert(`Human won.`)
+    }
+    if (computerScore == 5) {
+        alert(`Computer won.`)
+    }
+    if (confirm(`Reset game?`)) resetGame()
+}
 
 function resetGame() {
-    computerChoice = "";
-    userChoice = "";
-    computerScore = 0;
-    userScore = 0;
-    roundInfo.textContent = "Click one of the buttons to make your choice.";
-    computerScoreText.innerText = computerScore;
-    userScoreText.innerText = userScore;
-    console.info("game reset")
-    alert(`Scores have been reset.`);
-};
-
-function getRandomChoice() {
-    switch (Math.floor(Math.random() * 3)) {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
-    }
-};
-
-function compareChoices(user, pc) {
-    let message = "";
-
-    if (pc === "Rock" && user === "Paper"                 // user wins
-        || pc === "Paper" && user === "Scissors"
-        || pc === "Scissors" && user === "Rock"
-    ) {
-        message = `You won!\n${user} beats ${pc}.`;
-        userScore++;
-    } else if (user === "Rock" && pc === "Paper"          // pc wins
-        || user === "Paper" && pc === "Scissors"
-        || user === "Scissors" && pc === "Rock"
-    ) {
-        message = `Computer won!\n${pc} beats ${user}.`;
-        computerScore++;
-    } else if (user === pc) {                               // draw
-        message = `That was a draw.\nYou both chose ${user}.`;
-    } else {
-        console.warn("nobody won and no draw?\nprobably user canned");
-    }
-
-    showScore(message);
-};
-
-function showScore(msg) {
-    console.log(`Computer: ${computerScore}\nYou: ${userScore}`);
-
-    if (userScore < 5 && computerScore < 5) {
-        roundInfo.innerText = msg;
-        computerScoreText.innerText = computerScore;
-        userScoreText.innerText = userScore;       
-    } else {
-        let winner = "";
-        userScore === 5 ? winner = "You" : winner = "The Computer";
-        alert(`${winner} won 5 games.`)
-        resetGame();
-    }
+    humanScore = 0
+    computerScore = 0
 }
